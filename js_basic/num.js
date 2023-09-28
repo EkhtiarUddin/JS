fb412322f9dd302eacfa6164d8cd391cc058d393
EkhtiarUddin <uddinikhtiar6@gmail.com>
2023-09-28 14:20:27 +0600

    basic js

---
+++ js_basic/num.js
@@ -0,0 +1,8 @@
+// let radius = 10;
+// let pi = 3.14;
+// // console.log(radius, pi);
+
+// //math operators
+// // let result= radius%3;
+// let result = pi * radius **2;
+// console.log(result);
---
+++ js_basic/sendbox.js
@@ -0,0 +1,53 @@
+// // alert('hello, world');
+// // console.log(1);
+// // console.log(2)
+
+// // let age = 22;
+// // let year = 2001;
+// // console.log(age, year);
+// // age = 30;
+// // console.log(age);
+
+// // const points = 100;
+// // console.log(points)
+
+// // var score=29;
+// // console.log(score);
+
+// //------data types //
+// // strings
+
+// // console.log('hello, world');
+// // let email='admin@admin.com';
+// // console.log(email);
+
+// // strings concatenation
+// let firstName = 'Samira';
+// let lastName = 'Binte';
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// // strings character
+// console.log(fullName[2]);
+
+// // strings length 
+// console.log(fullName.length);
+
+// // strings methods
+// console.log(fullName.toUpperCase());
+// console.log(fullName.toLowerCase());
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
+
+// // let index=email.indexOf('@');
+// // console.log(index);
+
+// //common string methods
+// let email = 'mari@faku.uk';
+// // let res = email.indexOf('u');
+// // let res = email.slice(2,5);
+// // let res = email.substr(0,4);
+// // let res = email.replace('m', 'w');
+// let res = email.replace('a', 'o');
+// console.log(res);
+
