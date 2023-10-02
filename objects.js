d1db10ebb63384abfdcf27fd86a706e02bde1182
EkhtiarUddin <uddinikhtiar6@gmail.com>
2023-10-02 13:38:56 +0600

    update

---
+++ objects.js
@@ -0,0 +1,10 @@
+//object literals
+
+let user = {
+    name: 'meer',
+    age: 30,
+    email: 'meer@gmail.com',
+    location: 'dhaka',
+    blogs: ['make js blog', 'things to knnow before start js']
+};
+console.log(user);
